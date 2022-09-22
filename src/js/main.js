45286b316e82cba8c9d7e3365147ac9d0b1aad36
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-22 08:22:51 +0430

    Add event starter script

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,26 @@
 const navigator = document.querySelectorAll('.navigator');
-navigator.array.forEach((item) => {
+navigator.forEach((item) => {
   item.addEventListener('click', (element) => {
     const { target } = element;
-    switch (target.toLowerCase) {
+    const dataState = target.getAttribute('data-display').toLowerCase();
+    switch (dataState) {
       case 'daily':
         {
+          displayData(dataState);
         }
         break;
       case 'weekly':
         {
+          displayData(dataState);
         }
         break;
       case 'monthly': {
+        displayData(dataState);
       }
     }
   });
 });
 
 function displayData(dataState = 'daily') {
-    
+  console.log(dataState);
 }
